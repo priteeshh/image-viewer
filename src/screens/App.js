159a23f28788b6442cc9b60bd79c3773fec183df
priteeshh <priteeshh@gmail.com>
2020-07-11 05:12:17 +0530

    Routing implemented

---
+++ src/screens/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import Home from './home/Home'; 
+import Login from './login/Login'; 
+
+class App extends Component { 
+render() { 
+	return ( 
+	<Router> 
+		<div className="App"> 
+            <Route exact path='/' component={Login}></Route> 
+			<Route exact path='/home' component={Home}></Route> 
+		</div> 
+	</Router> 
+); 
+} 
+} 
+
+export default App; 
