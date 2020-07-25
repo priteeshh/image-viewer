f407ae34bcd2d97167b01a1858e75d26929fea15
priteeshh <priteeshh@gmail.com>
2020-07-26 00:15:40 +0530

    update full name modal added

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -5,7 +5,24 @@ import Typography from '@material-ui/core/Typography';
 import EditIcon from '@material-ui/icons/Edit';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import Modal from 'react-modal';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 // const tileData = [
 //     {
 //         img: "logo192.png",
@@ -44,14 +61,17 @@ import GridListTile from '@material-ui/core/GridListTile';
 //     }
 // ];
 class Profile extends Component {
-        constructor() {
+    constructor() {
         super();
         this.state = {
-            username : '',
-            media_count : 0,
-            profile : [],
-            images: []
-            
+            modalIsOpen: false,
+            fullname: 'Preeteesh Remalli',
+            username: '',
+            media_count: 0,
+            profile: [],
+            images: [],
+            updateFullName: ''
+
         };
     }
     logoutHandler = () => {
@@ -59,6 +79,23 @@ class Profile extends Component {
     }
     homeHandler = (e) => {
         this.props.history.push("/home");
+    }
+    closeModalHandler = () => {
+        this.setState({ modalIsOpen: false });
+    }
+    modalHandler = () => {
+        this.setState({
+            modalIsOpen: true
+        });
+    }
+    changeHandler = (e) => {
+        e.target.id === 'fullname' && this.setState({ updateFullName: e.target.value })
+    }
+    fullNameChangeHandler = () => {
+        this.setState({
+            fullname: this.state.updateFullName,
+            modalIsOpen: false
+        })
     }
     componentDidMount() {
         let thisComponent = this;
@@ -69,14 +106,16 @@ class Profile extends Component {
                 let responseData = JSON.parse(this.response);
                 console.log(responseData.username)
                 thisComponent.setState({ username: responseData.username })
-                thisComponent.setState({ profile: {
-                    username : responseData.username,
-                    media_count : responseData.media_count
-                } })
+                thisComponent.setState({
+                    profile: {
+                        username: responseData.username,
+                        media_count: responseData.media_count
+                    }
+                })
 
-                
 
-            } 
+
+            }
         });
         //Get Request for API
         xhrUserData.open('GET', 'https://graph.instagram.com/me?fields=id,username,media_count,account_type&access_token=IGQVJYcXNneUpFbFNRZAVIwTXBvdUVoaUJ2aldHVzVIcXQxa2pqenFxTUFmX3UxMnh4OUJ0bm9feG5hQjVuSTVmYUZA4bGhOcGF1T3RsUmlhZADB4SDZAuN3hMMUdSRlp3RnZAfbFNKWVJvYXZANNmM2Y2dsTwZDZD');
@@ -87,7 +126,7 @@ class Profile extends Component {
         xhrImageData.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 let responseData = JSON.parse(this.response).data;
-                 console.log(responseData);
+                console.log(responseData);
                 responseData.forEach(imageDetails => {
                     images.push({
                         id: imageDetails.id,
@@ -118,7 +157,7 @@ class Profile extends Component {
                             <div className="info-middle">
                                 <Typography variant="body2" component="p">
                                     Posts : {this.state.profile.media_count}
-                            </Typography>
+                                </Typography>
                                 <Typography variant="body2" component="p">
                                     Follow : 6
                             </Typography>
@@ -128,9 +167,9 @@ class Profile extends Component {
                             </div>
                             <div className="fullName">
                                 <Typography variant="subtitle1" component="p">
-                                    Upgrad Education
-                            </Typography>
-                                <button color="secondary" variant="contained" className="editBtn">
+                                    {this.state.fullname}
+                                </Typography>
+                                <button color="secondary" variant="contained" className="editBtn" onClick={this.modalHandler}>
                                     <EditIcon fontSize="small"></EditIcon>
                                 </button>
                             </div>
@@ -138,18 +177,30 @@ class Profile extends Component {
                         </div>
                     </div>
                     <div className="profile-images">
-                            <GridList cellHeight={350} cols={3}>
-                                {this.state.images.map((image) => (
-                                    <GridListTile key={image.id} cols={image.cols || 1}>
-                                        <img className="image-grid" src={image.media_url} alt={image.id} />
-                                    </GridListTile>
-                                ))}
-                            </GridList>
+                        <GridList cellHeight={350} cols={3}>
+                            {this.state.images.map((image) => (
+                                <GridListTile key={image.id} cols={image.cols || 1}>
+                                    <img className="image-grid" src={image.media_url} alt={image.id} />
+                                </GridListTile>
+                            ))}
+                        </GridList>
                     </div>
                     <div>
 
                     </div>
                 </div>
+
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login"
+                    onRequestClose={this.closeModalHandler} style={customStyles}>
+                    <Typography variant="h5">
+                        Edit
+                    </Typography>
+                    <FormControl>
+                        <InputLabel required htmlFor="fullname">Full name</InputLabel>
+                        <Input id="fullname" type="text" fullname={this.state.fullname} onChange={this.changeHandler}></Input>
+                    </FormControl><br/><br/>
+                    <Button variant="contained" color="primary" onClick={this.fullNameChangeHandler}>Update</Button>
+                </Modal>
             </div>
         )
     }
